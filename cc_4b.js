d0d27f01c0955324b2a44f35ebfbc96e274dde26
Nikita Siniayev <ns21@usf.edu>
2025-09-17 19:44:11 -0400

    first commit

---
+++ cc_4b.js
@@ -0,0 +1,8 @@
+let products = [
+    { sku: "001", name: "Apple", category: "Fruit", price: 1, inventory: 100 },
+    { sku: "002", name: "Banana", category: "Fruit", price: 0.5, inventory: 150 },
+    { sku: "003", name: "Carrot", category: "Vegetable", price: 0.8, inventory: 200 },
+    { sku: "004", name: "Broccoli", category: "Vegetable", price: 1.2, inventory: 80 },
+    { sku: "005", name: "Chicken Breast", category: "Meat", price: 5, inventory: 50 },
+];
+

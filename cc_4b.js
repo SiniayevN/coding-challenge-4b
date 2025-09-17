2326d7ea073afbed87ce678480b1b7df65742c89
Nikita Siniayev <ns21@usf.edu>
2025-09-17 19:58:43 -0400

    switch added

--- cc_4b.js
+++ cc_4b.js
@@ -1,8 +1,24 @@
 let products = [
     { sku: "001", name: "Apple", category: "Fruit", price: 1, inventory: 100 },
-    { sku: "002", name: "Banana", category: "Fruit", price: 0.5, inventory: 150 },
-    { sku: "003", name: "Carrot", category: "Vegetable", price: 0.8, inventory: 200 },
-    { sku: "004", name: "Broccoli", category: "Vegetable", price: 1.2, inventory: 80 },
-    { sku: "005", name: "Chicken Breast", category: "Meat", price: 5, inventory: 50 },
+    { sku: "002", name: "Banana", category: "Fruit", price: 2, inventory: 150 },
+    { sku: "003", name: "Carrot", category: "Vegetable", price: 3, inventory: 200 },
+    { sku: "004", name: "Broccoli", category: "Vegetable", price: 7, inventory: 80 },
+    { sku: "005", name: "Chicken Breast", category: "Meat", price: 15, inventory: 50 },
 ];
+//Step 2 done 
 
+let discount =0;
+for (let products of products) {
+    switch (products.category) {
+        case "Fruit":
+            discount = .2; 
+            break;
+        case "Vegetable":
+            discount = .15;
+            break;
+        case "Meat":
+            discount = .1; 
+            break;
+    }
+    
+}
